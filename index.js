a65bc201cd13805751b619c54c659c2451451408
hakshu <flyhighup.air25@gmail.com>
2020-05-20 05:10:23 +0900

    Add to send slack incoming webhook

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const trending = require("trending-github");
+const { IncomingWebhook } = require("@slack/webhook");
 
 /**
  ** Background Cloud Function.
@@ -9,7 +10,59 @@ const trending = require("trending-github");
 exports.githubTrendsNotify = (event, context) => {
   const term = event.term;
   const language = event.language;
+  const url = process.env.SLACK_WEBHOOK_URL;
+  const webhook = new IncomingWebhook(url);
+
   return trending(term, language)
-    .then((repos) => console.log(repos))
+    .then(async (repos) => {
+      const attachments = repos.map((repo) => buildRepoAttachment(repo));
+      const blocks = buildMainBlockMessage(term, language);
+      await webhook.send({ blocks, attachments });
+    })
     .catch((err) => console.error(err));
 };
+
+function buildMainBlockMessage(term, language) {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${term} ${language} Trends`,
+      },
+    },
+  ];
+}
+
+function buildRepoAttachment(repo) {
+  const blocks = buildBaseAttachmentBlock(repo);
+  if (repo.description) {
+    blocks.push({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: repo.description,
+      },
+    });
+  }
+  return { blocks };
+}
+
+function buildBaseAttachmentBlock(repo) {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `<${repo.href}|${repo.name}>`,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `:star: Stars: ${repo.stars}`,
+      },
+    },
+  ];
+}
