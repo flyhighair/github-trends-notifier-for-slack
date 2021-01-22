36cb011aa98560ae6a99b6777bd64c1efb068a81
hakshu <flyhighup.air25@gmail.com>
2021-01-23 08:29:32 +0900

    Add pubsub tf

---
+++ terraform/pub_sub.tf
@@ -0,0 +1,24 @@
+variable "pubsub_subscription_push_endpoint" {}
+
+resource "google_pubsub_topic" "github-trends-pubsub-topic" {
+  name    = "github-trends"
+  labels  = {}
+  timeouts {}
+}
+
+resource "google_pubsub_subscription" "github-trends-pubsub-subscription" {
+  ack_deadline_seconds       = 600
+  enable_message_ordering    = false
+  labels                     = {}
+  message_retention_duration = "604800s"
+  name                       = "gcf-github-trends-notify-asia-northeast1-github-trends"
+  retain_acked_messages      = false
+  topic                      = "projects/hakshu-private-project/topics/github-trends"
+
+  push_config {
+      attributes    = {}
+      push_endpoint = var.pubsub_subscription_push_endpoint
+  }
+
+  timeouts {}
+}
