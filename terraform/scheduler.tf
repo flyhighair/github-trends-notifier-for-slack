47584104627bb5062aac124a53a9b739161aa26b
hakshu <flyhighup.air25@gmail.com>
2021-01-23 08:37:02 +0900

    Add scheduler tf

---
+++ terraform/scheduler.tf
@@ -0,0 +1,13 @@
+resource "google_cloud_scheduler_job" "scheduler" {
+  name      = "github-trends-job"
+  schedule  = "0  9 * * 1"
+  time_zone = "Asia/Tokyo"
+
+  pubsub_target {
+      attributes = {}
+      data       = base64encode("{\"term\": \"Weekly\", \"language\": \"TypeScript\"}")
+      topic_name = google_pubsub_topic.github-trends-pubsub-topic.id
+  }
+
+  timeouts {}
+}
