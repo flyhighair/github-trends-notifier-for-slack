c89f2033363892234628f8b2ac69015c7d058c6d
hakshu <flyhighup.air25@gmail.com>
2021-01-23 11:56:01 +0900

    Add typescript src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const trending = require("trending-github");
-const { IncomingWebhook } = require("@slack/webhook");
+import trending from 'trending-github';
+import { IncomingWebhook } from '@slack/webhook';
+
+interface EventPayload {
+  data: string;
+}
 
 /**
  ** Background Cloud Function.
@@ -7,23 +11,28 @@ const { IncomingWebhook } = require("@slack/webhook");
  ** @param {object} event The event payload.
  ** @param {object} context The event metadata.
  **/
-exports.githubTrendsNotify = (event, context) => {
-  const { term, language } = JSON.parse(
+exports.githubTrendsNotify = (event: EventPayload, _: Object) => {
+  const { term, language }: { term: string, language: string} = JSON.parse(
     Buffer.from(event.data, "base64").toString()
   );
   const url = process.env.SLACK_WEBHOOK_URL;
+  if(!url) {
+    throw new Error('Not set env SLACK_WEBHOOK_URL');
+  }
+
   const webhook = new IncomingWebhook(url);
 
   return trending(term, language)
-    .then(async (repos) => {
+    .then(async (res) => {
+      const repos: Array<any> = res as Array<any>;
       const attachments = repos.map((repo) => buildRepoAttachment(repo));
       const blocks = buildMainBlockMessage(term, language);
       await webhook.send({ blocks, attachments });
     })
-    .catch((err) => console.error(err));
+    .catch((err: Error) => console.error(err));
 };
 
-function buildMainBlockMessage(term, language) {
+function buildMainBlockMessage(term: string, language: string) {
   return [
     {
       type: "section",
@@ -35,7 +44,7 @@ function buildMainBlockMessage(term, language) {
   ];
 }
 
-function buildRepoAttachment(repo) {
+function buildRepoAttachment(repo: any) {
   const blocks = buildBaseAttachmentBlock(repo);
   if (repo.description) {
     blocks.push({
@@ -49,7 +58,7 @@ function buildRepoAttachment(repo) {
   return { blocks };
 }
 
-function buildBaseAttachmentBlock(repo) {
+function buildBaseAttachmentBlock(repo: any) {
   return [
     {
       type: "section",
